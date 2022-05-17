2a5fc12605aee38325e538dca8a6c1d32c61bdf2
Phil Wang <lucidrains@gmail.com>
2022-05-17 08:05:21 -0700

    scaffold

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'enformer-jax',
+  packages = find_packages(exclude=[]),
+  version = '0.0.1',
+  license='MIT',
+  description = 'Enformer - Attention network for predicting gene expression, in Jax',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/lucidrains/enformer-jax',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'transformers',
+    'attention mechanism',
+    'gene expression'
+  ],
+  install_requires=[
+    'dm-haiku>=0.0.4',
+    'einops==0.4',
+    'jax>=0.3.4',
+    'jaxlib>=0.1',
+    'optax>=0.0.9',
+    'numpy'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
